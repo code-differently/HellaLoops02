8b71691397b5411ad150e675eacc1f071dc1e1bf
anthonyrobbins <arobbins612@gmail.com>
2021-11-09 14:28:21 -0500

    altctrldelete

--- src/main/java/Triangles.java
+++ src/main/java/Triangles.java
@@ -1,13 +1,26 @@
 public class Triangles {
     public String getTriangle(int numberOfRows) {
-        return null;
-    }
+        String starTriangle= "";
+        for (int a = 0; a < numberOfRows; a++) {
+            starTriangle += "*";
+            System.out.println(starTriangle);
+        }
+            return starTriangle;
+        }
+
 
     public String getRow(int numberOfStars) {
-        return null;
+        String starRow = "";
+        for(int i = 0; i < 20; i++) {
+            starRow += "*";
+            }
+        return starRow;
     }
 
     public String getSmallTriangle() {
+
+
+
         return null;
     }
 
