c2ad52c26ad90178959268ecbd5bbe32dbdedc3a
Faith Johnson <faithjohnson@Faiths-MacBook-Air.local>
2021-11-09 14:35:46 -0500

    finished

--- src/main/java/Triangles.java
+++ src/main/java/Triangles.java
@@ -1,17 +1,44 @@
 public class Triangles {
     public String getTriangle(int numberOfRows) {
-        return null;
+        StringBuilder tri = new StringBuilder();
+        for (int i = 1; i < numberOfRows; i++) {
+            for (int j = 1; j <= i; j++ ){
+                tri.append("*");
+            }
+            tri.append(System.lineSeparator());
+        }
+        return tri.toString();
     }
 
-    public String getRow(int numberOfStars) {
-        return null;
-    }
 
-    public String getSmallTriangle() {
-        return null;
-    }
+        public String getRow ( int numberOfStars){
+            StringBuilder row = new StringBuilder();
+            for (int i = 0; i < numberOfStars; i++) {
+                row.append("*");
+            }
+            return "" + row;
+        }
 
-    public String getLargeTriangle() {
-        return null;
-    }
+        public String getSmallTriangle () {
+        StringBuilder tri = new StringBuilder();
+        for (int i = 1; i < 5; i++){
+            for (int j = 1; j < i; j++) {
+                tri.append("*");
+            }
+            tri.append(System.lineSeparator());
+        }
+            return tri.toString();
+        }
+
+        public String getLargeTriangle () {
+        StringBuilder tri = new StringBuilder();
+        for (int i = 1; i<=9; i++){
+            for(int j = 1; j<=i; j++) {
+                tri.append("*");
+            }
+            tri.append(System.lineSeparator());
+        }
+            return tri.toString();
+        }
+
 }
